18b5c56e266bf3380ad04749cd36eac0f4091aa4
p.shamray <p.shamray@mserverone.com>
2016-02-02 17:01:10 +0200

    add move and rotate

---
+++ Assets/scripts/RespawnObjects.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RespawnObjects : MonoBehaviour {
+
+    public GameObject objI;
+    public GameObject objJ;
+    public GameObject objO;
+    public GameObject objT;
+    public GameObject objS;
+
+    private IList<GameObject> objsArray = new List<GameObject>();
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void Awake()
+    {
+        objsArray.Add(objI);
+        objsArray.Add(objJ);
+        objsArray.Add(objO);
+        objsArray.Add(objT);
+        objsArray.Add(objS);
+    }
+
+
+    public GameObject MakeObject() //Корутина должна возвращать IEnumerator 
+    {
+        int random = Mathf.CeilToInt(Random.value * objsArray.Count) - 1;
+
+        GameObject cl = (GameObject) Instantiate(objsArray[random], transform.position, transform.rotation);
+        cl.transform.rotation = new Quaternion(0, (Random.value > 0.5) ? 180 : 0, 0, 0);
+
+        return cl;        
+    }
+}
---
+++ Assets/scripts/TetrisObject.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TetrisObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.y < 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
