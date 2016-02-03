0f6a858f68220f2d50e5b8b88080f13741a272f0
p.shamray <p.shamray@mserverone.com>
2016-02-03 16:12:35 +0200

    change move

--- Assets/scripts/RespawnObjects.cs
+++ Assets/scripts/RespawnObjects.cs
@@ -4,24 +4,24 @@ using System.Collections.Generic;
 
 public class RespawnObjects : MonoBehaviour {
 
-    public GameObject objI;
-    public GameObject objJ;
-    public GameObject objO;
-    public GameObject objT;
-    public GameObject objS;
+    public GameObject[] group;
+    //public GameObject objJ;
+    //public GameObject objO;
+    //public GameObject objT;
+    //public GameObject objS;
 
     private GameObject currObject;
     private Rigidbody currObjectRb;
 
-    private IList<GameObject> objsArray = new List<GameObject>();
+    //private IList<GameObject> objsArray = new List<GameObject>();
     // Use this for initialization
     void Start()
     {
-        objsArray.Add(objI);
-        objsArray.Add(objJ);
-        objsArray.Add(objO);
-        objsArray.Add(objT);
-        objsArray.Add(objS);
+        //objsArray.Add(objI);
+        //objsArray.Add(objJ);
+        //objsArray.Add(objO);
+       // objsArray.Add(objT);
+        //objsArray.Add(objS);
 
         MakeRandomObject();
     }
@@ -36,15 +36,13 @@ public class RespawnObjects : MonoBehaviour {
 
     private void MakeRandomObject()
     {
-        int random = Mathf.CeilToInt(Random.value * objsArray.Count) - 1;
+        int random = Random.Range(0, group.Length);
         currObject = MakeObject(random);
         currObjectRb = currObject.GetComponent<Rigidbody>();
     }
 
     private GameObject MakeObject(int index) //Корутина должна возвращать IEnumerator 
     {
-        GameObject co = (GameObject) Instantiate(objsArray[index], transform.position, transform.rotation);
-        
-        return co;        
+       return (GameObject) Instantiate(group[index], transform.position, Quaternion.identity);        
     }
 }
--- Assets/scripts/TetrisObject.cs
+++ Assets/scripts/TetrisObject.cs
@@ -1,46 +1,103 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class TetrisObject : MonoBehaviour {
 
-    //private Rigidbody rb;
+    private Rigidbody rb;
 
-    private bool update = true;
+    private bool update = false;
 
-	// Use this for initialization
-	void Start () {
-        //rb = GetComponent<Rigidbody>();
-        transform.rotation = new Quaternion(0, (Random.value > 0.5) ? 180 : 0, 0, 0);
+    private bool btnRight = false;
+    private bool btnLeft = false;
+    private bool btnRotate = false;
+
+    private float rightBtnDelta;
+    private float leftBtnDelta;
+    private float rotateBtnDelta;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        transform.rotation = new Quaternion(0.0f, (Random.Range(0, 1) == 1 ? 180.0f : 0.0f), 0.0f, 0.0f);
+        update = true;
+
+        rightBtnDelta = 0;
+        leftBtnDelta = 0;
+        rotateBtnDelta = 0;
     }
 	
 	// Update is called once per frame
 	void Update () {
+        if (update)
+        {
+            rightBtnDelta += Time.fixedDeltaTime;
+            leftBtnDelta += Time.fixedDeltaTime;
+            rotateBtnDelta += Time.fixedDeltaTime;
 
+            if (rightBtnDelta > 0.1f)
+            {
+                rightBtnDelta = 0;
+                btnRight = Input.GetKey(KeyCode.RightArrow);
+            }
+            if (leftBtnDelta > 0.1f)
+            {
+                leftBtnDelta = 0;
+                btnLeft = Input.GetKey(KeyCode.LeftArrow);
+            }
+
+            if (rotateBtnDelta > 0.1f)
+            {
+                rotateBtnDelta = 0;
+                btnRotate = Input.GetKey(KeyCode.UpArrow);
+            }
+            /*
+            btnRight = Input.GetKey(KeyCode.RightArrow);
+            btnLeft = Input.GetKey(KeyCode.LeftArrow);
+            btnRotate = Input.GetKey(KeyCode.UpArrow);
+            */
+            if (btnRotate)
+            {
+                rb.transform.Rotate(new Vector3(0.0f, 0.0f, 90.0f));
+            }
+
+            if(btnRight || btnLeft)
+            {
+                rb.transform.position += new Vector3((btnRight) ? 1.0f : -1.0f, 0.0f, 0.0f);
+            }            
+
+            btnRight = false;
+            btnLeft = false;
+            btnRotate = false;
+        }
     }
 
     void FixedUpdate()
     {
-        if (update)
+        //rb.transform.position
+        if (transform.position.y < 0)
         {
-            bool moveRight = Input.GetButtonDown("Right");
-            bool moveLeft = Input.GetButtonDown("Left");
-            bool moveVertical = Input.GetButtonDown("Rotate");
-
-            Vector3 movement = new Vector3(0.0f, 0.0f, (float)((moveRight) ? 1.0f : ((moveLeft) ? -1.0f : 0.0f)));
-
-            transform.Rotate((moveVertical) ? 90 : 0, 0, 0);
-            transform.position += (movement);
-
-            if (transform.position.y < 0)
-            {
-                Destroy(gameObject);
-            }
+            Destroy(gameObject);
         }
     }
 
-    void OnCollisionEnter(Collision theCollision)
+    void OnCollisionStay(Collision collision)
     {
-        update = false;
-        Debug.Log(theCollision.gameObject.name);
+        if (update)
+        {
+            foreach (ContactPoint contact in collision.contacts)
+            {
+                /*
+                //print(contact.thisCollider.name + " hit " + contact.otherCollider.name);
+                Debug.DrawRay(contact.point, contact.normal, Color.white);
+
+                print("name: " + contact.thisCollider.name + " x: " + contact.point.x + " normal x: " + contact.normal.x);
+                print("name: " + contact.thisCollider.name + " y: " + contact.point.y + " normal y: " + contact.normal.y);
+                print("name: " + contact.thisCollider.name + " z: " + contact.point.z + " normal z: " + contact.normal.z);
+                */
+                if (contact.normal.y > 0.5f)
+                {
+                    update = false;
+                }
+            }
+        }
     }
 }
